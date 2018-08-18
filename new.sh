ad06a3006edb80b06df2db2b58a1fd1bce3c0b24
buffer0verflow <39618280+happypony9@users.noreply.github.com>
2018-08-18 17:27:30 -0500

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,23 +1,23 @@
 
 #multi
-screen -dmS a python firefly -p 25465 -t 50 107.174.19.150;
-screen -dmS b python firefly -p 25465 -t 50 107.174.19.154;
-screen -dmS c python firefly -p 25465 -t 50 172.107.165.3;
-screen -dmS d python firefly -p 25465 -t 50 172.107.165.150;
-screen -dmS e python firefly -p 25465 -t 50 172.107.165.151;
-screen -dmS f python firefly -p 25465 -t 50 172.107.165.160;
-screen -dmS g python firefly -p 25465 -t 50 172.93.238.111;
-screen -dmS h python firefly -p 25465 -t 50 172.93.238.133;
-screen -dmS i python firefly -p 25465 -t 50 172.93.238.140;
-screen -dmS j python firefly -p 25465 -t 50 172.93.238.190;
-screen -dmS k python firefly -p 25465 -t 50 172.93.238.201;
-screen -dmS l python firefly -p 25465 -t 50 176.31.103.7;
-screen -dmS m python firefly -p 25465 -t 50 45.35.54.3;
-screen -dmS n python firefly -p 25465 -t 50 45.35.54.5;
-screen -dmS o python firefly -p 25465 -t 50 45.35.54.52;
-screen -dmS p python firefly -p 25465 -t 50 45.35.54.55;
-screen -dmS q python firefly -p 25465 -t 50 45.35.54.57;
-screen -dmS r python firefly -p 25465 -t 50 45.35.54.118;
+screen -dmS a firefly -p 25465 -t 50 107.174.19.150;
+screen -dmS b firefly -p 25465 -t 50 107.174.19.154;
+screen -dmS c firefly -p 25465 -t 50 172.107.165.3;
+screen -dmS d firefly -p 25465 -t 50 172.107.165.150;
+screen -dmS e firefly -p 25465 -t 50 172.107.165.151;
+screen -dmS f firefly -p 25465 -t 50 172.107.165.160;
+screen -dmS g firefly -p 25465 -t 50 172.93.238.111;
+screen -dmS h firefly -p 25465 -t 50 172.93.238.133;
+screen -dmS i firefly -p 25465 -t 50 172.93.238.140;
+screen -dmS j firefly -p 25465 -t 50 172.93.238.190;
+screen -dmS k firefly -p 25465 -t 50 172.93.238.201;
+screen -dmS l firefly -p 25465 -t 50 176.31.103.7;
+screen -dmS m firefly -p 25465 -t 50 45.35.54.3;
+screen -dmS n firefly -p 25465 -t 50 45.35.54.5;
+screen -dmS o firefly -p 25465 -t 50 45.35.54.52;
+screen -dmS p firefly -p 25465 -t 50 45.35.54.55;
+screen -dmS q firefly -p 25465 -t 50 45.35.54.57;
+screen -dmS r firefly -p 25465 -t 50 45.35.54.118;
 #172.106.202.6#mpg
 #172.106.202.10
 #172.106.202.29
@@ -30,47 +30,47 @@ screen -dmS r python firefly -p 25465 -t 50 45.35.54.118;
 #172.106.202.117
 #172.106.202.119 -mcp fallout website
 #ftp
-screen -dmS s python firefly -p 21 -t 10 107.174.19.150;
-screen -dmS t python firefly -p 21 -t 10 107.174.19.154;
-screen -dmS u python firefly -p 21 -t 10 176.31.103.7;
-screen -dmS v python firefly -p 21 -t 10 176.31.103.228;
-screen -dmS w python firefly -p 21 -t 10 172.107.165.3;
-screen -dmS x python firefly -p 21 -t 10 172.107.165.150;
-screen -dmS y python firefly -p 21 -t 10 172.107.165.151;
-screen -dmS z python firefly -p 21 -t 10 172.107.165.165;
-screen -dmS aa python firefly -p 21 -t 10 172.107.165.198;
-screen -dmS bb python firefly -p 21 -t 10 172.93.238.111;
-screen -dmS cc python firefly -p 21 -t 10 172.93.238.133;
-screen -dmS dd python firefly -p 21 -t 10 172.93.238.140;
-screen -dmS ee python firefly -p 21 -t 10 172.93.238.153;
-screen -dmS ff python firefly -p 21 -t 10 172.93.238.190;
-screen -dmS gg python firefly -p 21 -t 10 172.93.238.201;
-screen -dmS hh python firefly -p 21 -t 10 45.35.54.3;
-screen -dmS ii python firefly -p 21 -t 10 45.35.54.5;
-screen -dmS jj python firefly -p 21 -t 10 45.35.54.14;
-screen -dmS kk python firefly -p 21 -t 10 45.35.54.35;
-screen -dmS ll python firefly -p 21 -t 10 45.35.54.52;
-screen -dmS mm python firefly -p 21 -t 10 45.35.54.55;
-screen -dmS nn python firefly -p 21 -t 10 45.35.54.57;
-screen -dmS oo python firefly -p 21 -t 10 45.35.54.77;
-screen -dmS pp python firefly -p 21 -t 10 45.35.54.118;
+screen -dmS s firefly -p 21 -t 10 107.174.19.150;
+screen -dmS t firefly -p 21 -t 10 107.174.19.154;
+screen -dmS u firefly -p 21 -t 10 176.31.103.7;
+screen -dmS v firefly -p 21 -t 10 176.31.103.228;
+screen -dmS w firefly -p 21 -t 10 172.107.165.3;
+screen -dmS x firefly -p 21 -t 10 172.107.165.150;
+screen -dmS y firefly -p 21 -t 10 172.107.165.151;
+screen -dmS z firefly -p 21 -t 10 172.107.165.165;
+screen -dmS aa firefly -p 21 -t 10 172.107.165.198;
+screen -dmS bb firefly -p 21 -t 10 172.93.238.111;
+screen -dmS cc firefly -p 21 -t 10 172.93.238.133;
+screen -dmS dd firefly -p 21 -t 10 172.93.238.140;
+screen -dmS ee firefly -p 21 -t 10 172.93.238.153;
+screen -dmS ff firefly -p 21 -t 10 172.93.238.190;
+screen -dmS gg firefly -p 21 -t 10 172.93.238.201;
+screen -dmS hh firefly -p 21 -t 10 45.35.54.3;
+screen -dmS ii firefly -p 21 -t 10 45.35.54.5;
+screen -dmS jj firefly -p 21 -t 10 45.35.54.14;
+screen -dmS kk firefly -p 21 -t 10 45.35.54.35;
+screen -dmS ll firefly -p 21 -t 10 45.35.54.52;
+screen -dmS mm firefly -p 21 -t 10 45.35.54.55;
+screen -dmS nn firefly -p 21 -t 10 45.35.54.57;
+screen -dmS oo firefly -p 21 -t 10 45.35.54.77;
+screen -dmS pp firefly -p 21 -t 10 45.35.54.118;
 
-screen -dmS mc1 python firefly -p 25565 -t 10 45.35.54.3;
-screen -dmS mc2 python firefly -p 25565 -t 10 45.35.54.38;
-screen -dmS mc3 python firefly -p 25565 -t 10 45.35.54.55;
-screen -dmS mc4 python firefly -p 25565 -t 10 45.35.54.57;
-screen -dmS mc5 python firefly -p 25565 -t 10 45.35.54.65;
-screen -dmS mc6 python firefly -p 25565 -t 10 45.35.54.66;
-screen -dmS mc7 python firefly -p 25565 -t 10 45.35.54.69;
-screen -dmS mc8 python firefly -p 25565 -t 10 45.35.54.70;
-screen -dmS mc9 python firefly -p 25565 -t 10 45.35.54.71;
-screen -dmS mc10 python firefly -p 25565 -t 10 45.35.54.72;
-screen -dmS mc11 python firefly -p 25565 -t 10 45.35.54.73;
-screen -dmS mc12 python firefly -p 25565 -t 10 45.35.54.74;
-screen -dmS mc8 python firefly -p 25565 -t 10 45.35.54.75;
-screen -dmS mc9 python firefly -p 25565 -t 10 45.35.54.112;
-screen -dmS mc10 python firefly -p 25565 -t 10 45.35.54.113;
-screen -dmS mc11 python firefly -p 25565 -t 10 45.35.54.114;
+screen -dmS mc1 firefly -p 25565 -t 10 45.35.54.3;
+screen -dmS mc2 firefly -p 25565 -t 10 45.35.54.38;
+screen -dmS mc3 firefly -p 25565 -t 10 45.35.54.55;
+screen -dmS mc4 firefly -p 25565 -t 10 45.35.54.57;
+screen -dmS mc5 firefly -p 25565 -t 10 45.35.54.65;
+screen -dmS mc6 firefly -p 25565 -t 10 45.35.54.66;
+screen -dmS mc7 firefly -p 25565 -t 10 45.35.54.69;
+screen -dmS mc8 firefly -p 25565 -t 10 45.35.54.70;
+screen -dmS mc9 firefly -p 25565 -t 10 45.35.54.71;
+screen -dmS mc10 firefly -p 25565 -t 10 45.35.54.72;
+screen -dmS mc11 firefly -p 25565 -t 10 45.35.54.73;
+screen -dmS mc12 firefly -p 25565 -t 10 45.35.54.74;
+screen -dmS mc8 firefly -p 25565 -t 10 45.35.54.75;
+screen -dmS mc9 firefly -p 25565 -t 10 45.35.54.112;
+screen -dmS mc10 firefly -p 25565 -t 10 45.35.54.113;
+screen -dmS mc11 firefly -p 25565 -t 10 45.35.54.114;
 #172.106.202.117
 #172.106.202.118
 #172.106.202.119
