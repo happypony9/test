590b67af30c402d52ead3c8dff89c84df2de8378
buffer0verflow <39618280+happypony9@users.noreply.github.com>
2018-08-31 08:26:22 -0500

    Update new.sh

--- new.sh
+++ new.sh
@@ -7,45 +7,38 @@ screen -dmS mm firefly -p 21 -t 20 45.35.54.55;
 screen -dmS nn firefly -p 21 -t 20 45.35.54.57;
 screen -dmS oo firefly -p 21 -t 20 45.35.54.77;
 screen -dmS pp firefly -p 21 -t 20 45.35.54.118;
-
-
 screen -dmS w firefly -p 21 -t 20 172.107.165.3;
 screen -dmS x firefly -p 21 -t 20 172.107.165.150;
 screen -dmS y firefly -p 21 -t 20 172.107.165.151;
 screen -dmS z firefly -p 21 -t 20 172.107.165.165;
 screen -dmS aa firefly -p 21 -t 20 172.107.165.198;
-
-
 screen -dmS c firefly -p 25465 -t 20 172.107.165.3;
 screen -dmS d firefly -p 25465 -t 20 172.107.165.150;
 screen -dmS g firefly -p 25465 -t 20 172.93.238.111;
-
 screen -dmS l firefly -p 25465 -t 20 176.31.103.7;
-
 screen -dmS m firefly -p 25465 -t 20 45.35.54.3;
 screen -dmS n firefly -p 25465 -t 20 45.35.54.5;
 screen -dmS o firefly -p 25465 -t 20 45.35.54.52;
 screen -dmS p firefly -p 25465 -t 20 45.35.54.55;
 screen -dmS q firefly -p 25465 -t 20 45.35.54.57;
 screen -dmS r firefly -p 25465 -t 20 45.35.54.118;
-
-screen -dmS r firefly -p 25465 -t 20 172.106.202.117
-screen -dmS r firefly -p 25465 -t 20 172.106.202.118
-screen -dmS r firefly -p 25465 -t 20 172.106.202.119
-screen -dmS r firefly -p 25465 -t 20 172.106.202.120
-screen -dmS r firefly -p 25465 -t 20 172.106.202.123
-screen -dmS r firefly -p 25465 -t 20 172.106.202.43
-screen -dmS r firefly -p 25465 -t 20 172.106.202.45
-screen -dmS r firefly -p 25465 -t 20 172.106.202.46
-screen -dmS r firefly -p 25465 -t 20 172.106.202.34
-screen -dmS r firefly -p 25465 -t 20 172.106.202.33
-screen -dmS r firefly -p 25465 -t 20 172.106.202.29
-screen -dmS r firefly -p 25465 -t 20 172.106.202.13
-screen -dmS r firefly -p 25465 -t 20 172.106.202.82
-screen -dmS r firefly -p 25465 -t 20 172.106.202.84
-screen -dmS 1 firefly -p 25465 -t 20 172.106.202.6
-screen -dmS 1 firefly -p 25465 -t 20 172.106.202.10
-screen -dmS 1 firefly -p 25465 -t 20 172.106.202.36
+screen -dmS r firefly -p 25465 -t 20 172.106.202.117;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.118;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.119;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.120;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.123;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.43;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.45;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.46;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.34;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.33;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.29;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.13;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.82;
+screen -dmS r firefly -p 25465 -t 20 172.106.202.84;
+screen -dmS 1 firefly -p 25465 -t 20 172.106.202.6;
+screen -dmS 1 firefly -p 25465 -t 20 172.106.202.10;
+screen -dmS 1 firefly -p 25465 -t 20 172.106.202.36;
 
 
 
